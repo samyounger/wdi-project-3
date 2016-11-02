ce3c450bfd6f6ecad7e28d52b4b6f7bd54de9607
samyounger <sam_younger@hotmail.com>
2016-11-02 12:23:45 +0000

    preparing the app for heroku deployment

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/novelistdb',
+  // db: process.env.MONGOLAB_URI || 'mongodb://localhost/novelistdb',
+  db: process.env.MONGOLAB_URI || 'mongodb://heroku_gktrp6lq:rscimcadkfrsc2prf4e8sou3tf@ds049496.mlab.com:49496/heroku_gktrp6lq',
   secret: process.env.SECRET || "novelists super secrets"
 };
